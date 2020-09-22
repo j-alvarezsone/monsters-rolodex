08d85a46aa9189b29286dd3a68876caf7cfc47f6
Jorge Alvarez <j.alvarezsone@gmail.com>
2020-09-22 16:05:10 +0200

    Updates

---
+++ precache-manifest.5d407a033a25780f20c70c910d5cc4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273db591ead60e345a778732d71136eb",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "9a37b73967e5d9ae5f1c",
+    "url": "/monsters-relodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "b962cb4a6b2f6b08cf78",
+    "url": "/monsters-relodex/static/js/2.d19f9663.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.d19f9663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a37b73967e5d9ae5f1c",
+    "url": "/monsters-relodex/static/js/main.30ab4fec.chunk.js"
+  },
+  {
+    "revision": "858dbcd1760996d62d88",
+    "url": "/monsters-relodex/static/js/runtime-main.a1871825.js"
+  }
+]);
